5fc6bd73fbf64d283a132c3a4f74bf452c5bf025
JD <jake.crandell@snhu.edu>
2025-11-06 21:36:23 -0600

    renaming parsers

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -4,9 +4,9 @@ SecretHound Parsers Module
 This module contains parsers for various secret scanning tools.
 """
 
-from .github_secret_scanner import GitHubSecretScannerParser
-from .nosey_parker import NoseyParkerParser
+from .github import GitHubSecretScannerParser
+from .noseyparker import NoseyParkerParser
 from .trufflehog import TruffleHogParser
-from .nemesis_parser import NemesisParser
+from .nemesis import NemesisParser
 
 __all__ = ['GitHubSecretScannerParser', 'NoseyParkerParser', 'TruffleHogParser', 'NemesisParser']
